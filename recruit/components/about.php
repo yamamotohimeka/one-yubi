da4959013a903c40fe395cdfb74b246f414791f7
yamamotohimeka <tribestaff0113@gmail.com>
2025-01-24 13:11:33 +0900

    SP　改行調節

--- recruit/components/about.php
+++ recruit/components/about.php
@@ -6,21 +6,20 @@
       <ul class="about__point__list">
         <li class="about__point__list-item fadeUp1">
           <img src="<?php echo $path; ?>assets/images/about-point1.png" alt="ポイント1">
-          <p>未経験でも大丈夫です！ほとんどの女の子が未経験からのスタート<br class="pc">
-            見たり・触ったりするだけなので講習もなしです！<br>
+          <p>未経験でも大丈夫です！ほとんどの<br class="tab">女の子が未経験からのスタート<br>
+            見たり・触ったりするだけなので<br class="tab">講習もなしです！<br>
             面接後最速30分で入店可能です</p>
         </li>
         <li class="about__point__list-item fadeUp2">
           <img src="<?php echo $path; ?>assets/images/about-point2.png" alt="ポイント2">
-          <p>受付型のお店なので自分が接客するお客様の顔は<br class="pc">
-            先に確認できます身バレの心配一切なしです。<br>
-            又完全個室待機なので他の女の子に会う事もございません。</p>
+          <p>受付型のお店なので自分が接客する<br class="tab">お客様の顔は<br class="pc">先に確認できます<br class="tab">身バレの心配一切なしです。<br>
+            又完全個室待機なので他の女の子に<br class="tab">会う事もございません。</p>
         </li>
         <li class="about__point__list-item fadeUp3">
           <img src="<?php echo $path; ?>assets/images/about-point3.png" alt="ポイント3">
-          <p>30代が活躍できるお店なので既婚者の方は勿論シンママの方も<br class="pc">
-            大歓迎です。週に1日3時間以上の出勤であれば大丈夫！<br class="pc">
-            掛け持ちしながら即日お給料がもらえます。</p>
+          <p>30代が活躍できるお店なので<br class="tab">既婚者の方は勿論シンママの方も<br>
+            大歓迎です。週に1日3時間以上の<br class="tab">出勤であれば大丈夫！<br>
+            掛け持ちしながら即日お給料が<br class="tab">もらえます。</p>
         </li>
       </ul>
 
@@ -56,7 +55,7 @@
           </dt>
           <dd>
             稼げないとかは考えなくても大丈夫です。<br>
-            3時間の勤務から日給保証をご用意しております。
+            3時間の勤務から日給保証を<br class="tab">ご用意しております。
           </dd>
         </div>
       </dl>
