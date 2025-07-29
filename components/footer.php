a17261f303d6cb1c4613d7597e41de7f75d8e777
nakaya-tribe <tribestaff0105@gmail.com>
2025-07-29 15:43:10 +0900

    ソースコードを整形

--- components/footer.php
+++ components/footer.php
@@ -1,13 +1,8 @@
 <footer class="footer">
-
-
   <div class="container pc">
-
-
     <div class="footer__logo">
       <img src="<?php echo $path; ?>assets/img/logo.png" alt="お姉さんの指先">
     </div>
-
   </div>
 
   <div class="footer__bg">
@@ -17,46 +12,44 @@
           <ul class="footer__nav__list">
             <li class="footer__nav__list-link <?php if ($templete === 'top') echo 'active'; ?>">
               <a href="<?php echo $path; ?>">
-                <p>TOP</p>
+                <span>TOP</span>
               </a>
             </li>
-
             <li class="footer__nav__list-link <?php if ($templete === 'concept') echo 'active'; ?>">
               <a href="<?php echo $path; ?>concept.php">
-                <p>CONCEPT</p>
+                <span>CONCEPT</span>
               </a>
             </li>
-            <li class="footer__nav__list-link <?php if ($templete=== 'cast') echo 'active'; ?>">
+            <li class="footer__nav__list-link <?php if ($templete === 'cast') echo 'active'; ?>">
               <a href="<?php echo $path; ?>girl.php">
-                <p>CAST</p>
+                <span>CAST</span>
               </a>
             </li>
             <li class="footer__nav__list-link <?php if ($templete === 'diary') echo 'active'; ?>">
               <a href="<?php echo $path; ?>diary.php">
-                <p>PHOTOBBS</p>
+                <span>PHOTOBBS</span>
               </a>
             </li>
             <li class="footer__nav__list-link <?php if ($templete === 'schedule') echo 'active'; ?>">
               <a href="<?php echo $path; ?>schedule.php">
-                <p>SCHEDULE</p>
+                <span>SCHEDULE</span>
               </a>
             </li>
             <li class="footer__nav__list-link <?php if ($templete === 'system') echo 'active'; ?>">
               <a href="<?php echo $path; ?>system.php">
-                <p>SYSTEM</p>
+                <span>SYSTEM</span>
               </a>
             </li>
             <li class="footer__nav__list-link <?php if ($templete === 'access') echo 'active'; ?>">
               <a href="<?php echo $path; ?>access.php">
-                <p>ACCESS</p>
+                <span>ACCESS</span>
               </a>
             </li>
             <li class="footer__nav__list-link <?php if ($templete === 'recruit') echo 'active'; ?>">
               <a href="https://one-yubi.net/" target="_blank">
-                <p>RECRUIT</p>
+                <span>RECRUIT</span>
               </a>
             </li>
-
           </ul>
         </nav>
 
@@ -65,116 +58,123 @@
           <a href="tel:0120-868-846">0120-868-846</a>
         </div>
       </div>
-
-
-
     </div>
   </div>
 
   <div class="container pc">
     <div class="footer__banner">
       <a rel="nofollow" href="https://kansai.qzin.jp/oneyubi/?v=official" target="_blank">
-        <img src="https://ad.qzin.jp/img/vanilla468-60.gif" width="468" border="0" alt="お姉さんの指先で稼ぐならバニラ求人"></a>
-      <a href="https://www.bwork.net/" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/bwork88x31.gif" alt="Bwork"></a>
+        <img src="https://ad.qzin.jp/img/vanilla468-60.gif" width="468" border="0" alt="お姉さんの指先で稼ぐならバニラ求人">
+      </a>
+      <a href="https://www.bwork.net/" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/bwork88x31.gif" alt="Bwork">
+      </a>
       <a href="https://q-pri.com/shop_list/area/102/104" target="_blank" rel="noopener">
-        <img src="<?php echo $path; ?>assets/img/banner/q-puri200x40.png" alt="Qプリ"></a>
-      <a href="https://q-pri.com/shop_list/area/102/104" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/q-puri468x60.png" alt="Qプリ"></a>
-      <a href="https://q-pri.com/shop_list/area/102/104" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/q-puri640x100.png" alt="Qプリ"></a>
-      <a href="https://www.r-30.net/kansai/" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/r-net88-31.jpg" alt="R-30.net"></a>
-      <a href="https://www.r-30.net/kansai/" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/r-net192-53.gif" alt="R-30.net"></a>
-      <a href="https://www.r-30.net/kansai/" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/r-net468-60.gif" alt="R-30.net"></a>
-      <a href="http://kirajob.com" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/kirakira.jpg" alt="キラキラジョブ"></a>
-      <a href="https://happyhellowork.com/rank.php?s=4" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/hhw.gif" alt="ハッピーハローワーク"></a>
-      <a href="https://work-mikke.jp/" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/mikke.jpg" alt="みっけ"></a>
-      <a href="https://kansai-fuu.info/" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/oosakafuzoku.jpg" alt="大阪風俗求人情報"></a>
-      <a href="https://www.dekasegichan.com/" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/dekasegichan.jpg" alt="出稼ぎちゃん"></a>
-      <a href="http://dekasegifuzoku.com/" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/dekasegi-com.gif" alt="出稼ぎドットコム"></a>
-      <a href="https://no-fuzoku.com/" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/fuzokujanai.jpg" alt="風俗じゃない！日払い高収入求人"></a>
-      <a href="https://ona-club.com/" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/onakura.gif" alt="オナクラ風俗.com"></a>
-      <a href="https://www.fucolle.com/kansai/" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/fukore.gif" alt="フーコレ関西"></a>
-      <a href="https://fujoho.jp/" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/kutikomi.gif" alt="口コミ情報局"></a>
-      <a href="https://www.tekoki-no1.com/" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/kutikomi.gif" alt="手コキが一番"></a>
-      <a href="https://www.tekoki-fuzoku-joho.com/" target="_blank" rel="noopener"><img
-          src="<?php echo $path; ?>assets/img/banner/maniax.jpg" alt="手コキ風俗マニアックス"></a>
+        <img src="<?php echo $path; ?>assets/img/banner/q-puri200x40.png" alt="Qプリ">
+      </a>
+      <a href="https://q-pri.com/shop_list/area/102/104" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/q-puri468x60.png" alt="Qプリ">
+      </a>
+      <a href="https://q-pri.com/shop_list/area/102/104" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/q-puri640x100.png" alt="Qプリ">
+      </a>
+      <a href="https://www.r-30.net/kansai/" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/r-net88-31.jpg" alt="R-30.net">
+      </a>
+      <a href="https://www.r-30.net/kansai/" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/r-net192-53.gif" alt="R-30.net">
+      </a>
+      <a href="https://www.r-30.net/kansai/" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/r-net468-60.gif" alt="R-30.net">
+      </a>
+      <a href="http://kirajob.com" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/kirakira.jpg" alt="キラキラジョブ">
+      </a>
+      <a href="https://happyhellowork.com/rank.php?s=4" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/hhw.gif" alt="ハッピーハローワーク">
+      </a>
+      <a href="https://work-mikke.jp/" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/mikke.jpg" alt="みっけ">
+      </a>
+      <a href="https://kansai-fuu.info/" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/oosakafuzoku.jpg" alt="大阪風俗求人情報">
+      </a>
+      <a href="https://www.dekasegichan.com/" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/dekasegichan.jpg" alt="出稼ぎちゃん">
+      </a>
+      <a href="http://dekasegifuzoku.com/" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/dekasegi-com.gif" alt="出稼ぎドットコム">
+      </a>
+      <a href="https://no-fuzoku.com/" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/fuzokujanai.jpg" alt="風俗じゃない！日払い高収入求人">
+      </a>
+      <a href="https://ona-club.com/" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/onakura.gif" alt="オナクラ風俗.com">
+      </a>
+      <a href="https://www.fucolle.com/kansai/" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/fukore.gif" alt="フーコレ関西">
+      </a>
+      <a href="https://fujoho.jp/" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/kutikomi.gif" alt="口コミ情報局">
+      </a>
+      <a href="https://www.tekoki-no1.com/" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/kutikomi.gif" alt="手コキが一番">
+      </a>
+      <a href="https://www.tekoki-fuzoku-joho.com/" target="_blank" rel="noopener">
+        <img src="<?php echo $path; ?>assets/img/banner/maniax.jpg" alt="手コキ風俗マニアックス">
+      </a>
     </div>
   </div>
 
   <div class="container tab">
-
-
-
     <div class="footer__nav pc">
       <ul class="footer__nav__list">
         <li class="footer__nav__list-link">
           <a href="<?php echo $path; ?>" class="footer__link">
-            <p>TOP</p>
+            <span>TOP</span>
           </a>
         </li>
         <li class="border"></li>
         <li class="footer__nav__list-link">
           <a href="<?php echo $path; ?>girl.php" class="footer__link">
-            <p>女の子一覧
-            </p>
+            <span>女の子一覧</span>
           </a>
         </li>
         <li class="border"></li>
         <li class="footer__nav__list-link">
           <a href="<?php echo $path; ?>schedule.php" class="footer__link">
-            <p>出勤情報</p>
+            <span>出勤情報</span>
           </a>
         </li>
         <li class="border"></li>
         <li class="footer__nav__list-link">
           <a href="<?php echo $path; ?>system.php" class="footer__link">
-            <p>システム</p>
+            <span>システム</span>
           </a>
         </li>
         <li class="border"></li>
         <li class="footer__nav__list-link">
           <a href="<?php echo $path; ?>event.php" class="footer__link">
-            <p>イベント</p>
+            <span>イベント</span>
           </a>
         </li>
         <li class="border"></li>
         <li class="footer__nav__list-link">
           <a href="<?php echo $path; ?>access.php" class="footer__link">
-            <p>アクセス</p>
+            <span>アクセス</span>
           </a>
         </li>
         <li class="border"></li>
         <li class="footer__nav__list-link">
           <a href="../recruit" class="footer__link" target="_blank">
-            <p>求人サイト</p>
+            <span>求人サイト</span>
           </a>
         </li>
       </ul>
     </div>
   </div>
 
-  </div>
-
-
-
   <div class="container tab">
-
-
     <div class="footer__tab__nav">
       <a href="tel:0120-868-846" class="footer__tab__nav__btn">
         <img src="<?php echo $path; ?>assets/img/telNav-btn.png" alt="電話">
@@ -183,24 +183,18 @@
         <img src="<?php echo $path; ?>assets/img/menuNav-btn.png" alt="電話">
       </div>
     </div>
-
   </div>
+
   <div class="footer__topBtn tab">
     <a href="<?php echo $path; ?>">TOPへ</a>
   </div>
-
 </footer>
-
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/locale/ja.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/protonet-jquery.inview/1.1.2/jquery.inview.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 <script type="text/javascript" src="<?php echo $path; ?>/assets/js/index.js"></script>
-
-
-
-
 
 </body>
 
